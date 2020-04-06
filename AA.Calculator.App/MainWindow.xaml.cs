dbb8e562a809c8233d1a4ba47cf0e0dbaae7de6f
Alan <alanbiegun13@gmail.com>
2020-04-06 18:29:27 +0200

    a

--- AA.Calculator.App/MainWindow.xaml.cs
+++ AA.Calculator.App/MainWindow.xaml.cs
@@ -20,36 +20,26 @@ namespace AA.Calculator.App
 	/// <summary>
 	/// Interaction logic for MainWindow.xaml
 	/// </summary>
-	public partial class MainWindow : Window, INotifyPropertyChanged
+	public partial class MainWindow : Window
 	{
-		public string equation;
-		public string Equation
-		{
-			get => equation; 
-			set
-			{
-				equation = value;
-				OnPropertyChanged("Equation");
-			}
-		}
-		public string input;
-		public string Input {
-			get => input;
-			set
-			{
-				input = value;
-				OnPropertyChanged("Input");
-			}
-		}
+		public string LastSign { get; set; }
+		public string LastNumber { get; set; }
+		public bool UseLast { get; set; }
+		public string LastResult { get; set; }
 
 		public Interpreter InterpreterOnp { get; set; }
 
 		public MainWindow()
 		{
-			InterpreterOnp = new Interpreter();
-			Equation = "";
-			Input = "";
 			InitializeComponent();
+
+			InterpreterOnp = new Interpreter();
+			Equation.Text = "";
+			Input.Text = "";
+			LastSign = "";
+			LastNumber = "";
+			LastResult = "";
+			UseLast = false;
 		}
 
 		private void FunctionClick(object sender, RoutedEventArgs e)
@@ -59,39 +49,82 @@ namespace AA.Calculator.App
 
 		private void NumberClick(object sender, RoutedEventArgs e)
 		{
+			if (UseLast)
+			{
+				Equation.Text = Input.Text;
+			}
+			UseLast = false;
 			var button = sender as Button;
-			Input += button.Content;
+			Input.Text += button.Content;
+			LastNumber = (string)button.Content;
 		}
 
 		private void SignClick(object sender, RoutedEventArgs e)
 		{
-			Equation += Input;
+			if (UseLast)
+			{
+				Equation.Text = Input.Text;
+			}
+			else
+			{
+				Equation.Text += Input.Text;
+			}
+			UseLast = false;
 			var button = sender as Button;
 			ClearCurrentValue(sender, e);
-			Equation += button.Content;
+			Equation.Text += button.Content;
+			LastSign = (string)button.Content;
 		}
 
 		private void Calculate(object sender, RoutedEventArgs e)
 		{
-			Equation += Input;
-			Input = InterpreterOnp.Calculate(Equation).ToString();
+			if (UseLast)
+			{
+				Equation.Text = Input.Text;
+				Equation.Text += LastSign;
+				Equation.Text += LastNumber;
+			}
+			else
+			{
+				if (string.IsNullOrEmpty(Input.Text) && string.IsNullOrEmpty(LastResult))
+				{
+					Input.Text = LastNumber;
+
+				}
+				else if (string.IsNullOrEmpty(Input.Text) && !string.IsNullOrEmpty(LastResult))
+				{
+					LastNumber = LastResult;
+					Input.Text = LastResult;
+				}
+
+				Equation.Text += Input.Text;
+			}
+
+			try
+			{
+				Input.Text = LastResult = InterpreterOnp.Calculate(Equation.Text).ToString();
+
+			}
+			catch (Exception ex)
+			{
+				Input.Text = ex.Message;
+			}
+
+			UseLast = true;
 		}
 
 		private void ClearCurrentValue(object sender, RoutedEventArgs e)
 		{
-			Input = "";
+			Input.Text = "";
 		}
 
 		private void ClearEquation(object sender, RoutedEventArgs e)
 		{
-			Equation = "";
+			Equation.Text = "";
+			LastSign = "";
+			LastNumber = "";
+			LastResult = "";
 			ClearCurrentValue(sender, e);
-		}
-
-		public event PropertyChangedEventHandler PropertyChanged;
-		private void OnPropertyChanged(string name)
-		{
-			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
 		}
 	}
 }
